394aaf564094b5545ac01ba2123da187fdb604b1
jairdean <jairdean@hotmail.com>
2018-01-29 23:19:30 -0500

    MODULO 6 TIPO COLABORADOR

--- app/src/main/java/com/example/jairo/proyecto/ModTipoColaborador.java
+++ app/src/main/java/com/example/jairo/proyecto/ModTipoColaborador.java
@@ -2,16 +2,46 @@ package com.example.jairo.proyecto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ModTipoColaborador extends AppCompatActivity {
+
+    private Button btnIngresar,btnListar;
+    private EditText txt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mod_tipo_colaborador);
 
+        txt1 = findViewById(R.id.txt1);
+
+        btnIngresar = findViewById(R.id.btnIngresar);//boton ingresar
+        btnListar = findViewById(R.id.btnListar);//boton listar
 
 
+        //Implementamos el evento click del botón
+        //boton ingresar
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //aqui se pone la accion que haria el boton.
+
+
+            }
+        });
+
+        //boton listar
+        btnListar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //aqui se pone la accion que haria el boton.
+
+
+            }
+        });
 
     }//fin ON CREATE
 }
