4fca4ea5ce36454a4a040ef9f363392766fdf6c7
jairdean <jairdean@hotmail.com>
2018-01-30 10:53:41 -0500

    LISTA global de modulos

--- app/src/main/java/com/example/jairo/proyecto/ListActivity.java
+++ app/src/main/java/com/example/jairo/proyecto/ListActivity.java
@@ -2,15 +2,18 @@ package com.example.jairo.proyecto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class ListActivity extends AppCompatActivity {
     private String LISTARDATOSDE;
+    private TableLayout myData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-
 
         //Recuperamos la información pasada en el intent
         Bundle bundle = this.getIntent().getExtras();
@@ -18,6 +21,27 @@ public class ListActivity extends AppCompatActivity {
         //Construimos el mensaje a mostrar
         LISTARDATOSDE=bundle.getString("LISTARDATOSDE");
 
+
+                    myData = findViewById(R.id.lytJairoLista);
+        myData.setStretchAllColumns(true);
+        myData.bringToFront();
+        int i=0;
+        for( i= 0; i < 3; i++){
+            //crea la fila
+            TableRow tr =  new TableRow(this);
+            //obtener los datos de la BASE
+            TextView c1 = new TextView(this);
+            c1.setText(LISTARDATOSDE);
+            TextView c2 = new TextView(this);
+            c2.setText("dos "+i);
+            TextView c3 = new TextView(this);
+            c3.setText("tres "+i);
+            //agregar los datos en la tabla
+            tr.addView(c1);
+            tr.addView(c2);
+            tr.addView(c3);
+            myData.addView(tr);
+        }
 
     }
 }
