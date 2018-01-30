b5d191c2d278f1a1dab9c7da8776f1802eb2707a
jairdean <jairdean@hotmail.com>
2018-01-30 18:51:55 -0500

    correccion variable combo

--- app/src/main/java/com/example/jairo/proyecto/MainActivity.java
+++ app/src/main/java/com/example/jairo/proyecto/MainActivity.java
@@ -173,26 +173,25 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
 
     @Override
     public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-        // TODO Auto-generated method stub
         String sp1= String.valueOf(s1.getSelectedItem());
         Toast.makeText(this, sp1, Toast.LENGTH_SHORT).show();
         if(sp1.contentEquals("Income")) {
-            List<String> list = new ArrayList<String>();
+            List<String> list = new ArrayList<>();
             list.add("Salary");//You should add items from db here (first spinner)
             list.add("jairo");
 
-            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+            ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,
                     android.R.layout.simple_spinner_item, list);
             dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             dataAdapter.notifyDataSetChanged();
             s2.setAdapter(dataAdapter);
         }
         if(sp1.contentEquals("Expense")) {
-            List<String> list = new ArrayList<String>();
+            List<String> list = new ArrayList<>();
             list.add("Conveyance");//you should add items from db here(2nd spinner)
             list.add("Jhon");
 
-            ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,
+            ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<>(this,
                     android.R.layout.simple_spinner_item, list);
             dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             dataAdapter2.notifyDataSetChanged();
