c1f12bd770a922fc10bd520f9e4b8f7f38159125
jairdean <jairdean@hotmail.com>
2018-01-31 19:38:59 -0500

    boton main

--- app/src/main/java/com/example/jairo/proyecto/Modulo1.java
+++ app/src/main/java/com/example/jairo/proyecto/Modulo1.java
@@ -1,11 +1,15 @@
 package com.example.jairo.proyecto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Modulo1 extends AppCompatActivity {
     private TextView txtSaludo;
+    private Button btnvueltaMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +18,29 @@ public class Modulo1 extends AppCompatActivity {
 
         //Localizar los controles
         txtSaludo = findViewById(R.id.txtSaludo);
+        btnvueltaMain = findViewById(R.id.btnvueltaMain);
 
         //Recuperamos la información pasada en el intent
         Bundle bundle = this.getIntent().getExtras();
 
         //Construimos el mensaje a mostrar
         txtSaludo.setText("LISTA\n"+ bundle.getString("PASO_NOMBRE"));
-    }
+
+
+        btnvueltaMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creamos el Intent la comunicación entre los distintos componentes y aplicaciones en Android se realiza mediante intents
+                Intent intent = new Intent(Modulo1.this, MainActivity.class);
+                //Iniciamos la nueva actividad
+                startActivity(intent);
+            }
+        });
+    }//fin ON CREATE
+
+
+
+
+
+
 }
