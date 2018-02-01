fa1079a56a6e41b4387ae25b2c91d2fd0f6b28d8
jairdean <jairdean@hotmail.com>
2018-01-31 22:10:24 -0500

    ip global

--- app/src/main/java/com/example/jairo/proyecto/ListActivity.java
+++ app/src/main/java/com/example/jairo/proyecto/ListActivity.java
@@ -16,7 +16,7 @@ import org.ksoap2.transport.HttpTransportSE;
 
 public class ListActivity extends AppCompatActivity {
      String LISTARDATOSDE,strAum;
-String IPaqui="172.16.24.19";
+String IPaqui="192.168.100.171";
     TableLayout myData;
     TableRow tr;
     TextView c1,c2,c3;
--- app/src/main/java/com/example/jairo/proyecto/ModTipoColaborador.java
+++ app/src/main/java/com/example/jairo/proyecto/ModTipoColaborador.java
@@ -1,16 +1,24 @@
 package com.example.jairo.proyecto;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
 public class ModTipoColaborador extends AppCompatActivity {
 
     private Button btnIngresar,btnListar;
     private EditText txt1;
+    String IPaqui="192.168.100.171",strAum,recibotxt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +37,10 @@ public class ModTipoColaborador extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //aqui se pone la accion que haria el boton.
+                recibotxt1=txt1.getText().toString();//recupero la data
 
-
-
+                TareaListaTipoCol llamada= new TareaListaTipoCol();
+                llamada.execute();
 
                 //este de ley va al final porque se llama a la lista para ver el nuevo dato ingresado
                 invocar_lista();
@@ -59,5 +68,38 @@ public class ModTipoColaborador extends AppCompatActivity {
         intent.putExtras(b);
         //Iniciamos la nueva actividad
         startActivity(intent);
-    }
+    }//fin metodo
+
+    private class TareaListaTipoCol extends AsyncTask<String, Integer, Boolean> {
+        protected Boolean doInBackground(String... params) {
+            boolean resul = true;
+            final String NAMESPACE = "http://xmlns.jj.com/services/v1/ups";
+            final String URL = "http://"+IPaqui+":8080/WSDLandroid/proceso_wc";
+            final String METHOD_NAME = "insertarTipoColaborador";
+
+            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+
+            request.addProperty("descripcion",recibotxt1);
+            System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"+recibotxt1);
+
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.dotNet = false;
+            envelope.setOutputSoapObject(request);
+            HttpTransportSE transporte = new HttpTransportSE(URL);
+
+            try {
+                transporte.call(NAMESPACE + "/" + METHOD_NAME, envelope);
+                SoapPrimitive resultado_xml= (SoapPrimitive) envelope.getResponse();
+                String res=resultado_xml.toString();
+                if(!res.equals(1)){
+                    resul=false;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                resul = false;
+            }
+            return resul;
+        }
+    }//fin sub clase
+
 }
